0bd63e35bfa536cdfb626743bd8c108ce13d6a43
Enzo Defranco <enzolobod22@gmail.com>
2023-04-29 17:58:44 -0300

    Oculte los campos al momento de la consulta

--- app/src/main/java/com/example/tp2_punto2/ConsultarActivity.kt
+++ app/src/main/java/com/example/tp2_punto2/ConsultarActivity.kt
@@ -3,9 +3,11 @@ import android.view.View
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import android.content.Context
 
 class ConsultarActivity : AppCompatActivity() {
     lateinit var ciudadDBHELPER: miSQLiteHelper
@@ -15,12 +17,20 @@ class ConsultarActivity : AppCompatActivity() {
         setContentView(R.layout.consultar)
         ciudadDBHELPER = miSQLiteHelper(this)
 
+        findViewById<TextView>(R.id.ciudad).visibility = View.GONE
+        findViewById<TextView>(R.id.pais).visibility = View.GONE
+        findViewById<TextView>(R.id.poblacion).visibility = View.GONE
+
         var btn = findViewById<Button>(R.id.btnConsultar)
         btn.setOnClickListener{
             var consulta = findViewById<TextView>(R.id.ciudad_consulta).text.toString()
             val datalist = ciudadDBHELPER.getDataByCity(consulta)
             if (datalist != null){
+
                 findViewById<TextView>(R.id.ciudad_consulta).visibility = View.GONE
+                findViewById<TextView>(R.id.ciudad).visibility = View.VISIBLE
+                findViewById<TextView>(R.id.pais).visibility = View.VISIBLE
+                findViewById<TextView>(R.id.poblacion).visibility = View.VISIBLE
                 findViewById<TextView>(R.id.ciudad).text = "Ciudad consultada: " + datalist[0] as String
                 findViewById<TextView>(R.id.pais).text = "País: " + datalist[1] as String
                 findViewById<TextView>(R.id.poblacion).text = "Población aproximada: " + datalist[2].toString()
